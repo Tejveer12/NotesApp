6f8ebc54479897a0d51f72de354e933d5ee7eecf
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2022-01-07 11:46:09 +0530

    almost done

--- app/src/main/java/com/example/notesapp/SplashScreen.kt
+++ app/src/main/java/com/example/notesapp/SplashScreen.kt
@@ -2,10 +2,16 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.delay
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+//       Handler().postDelayed(Runnable {
+//
+//           startActivity(Intent(packageContext: this,MainActivity::class.java))
+//       })
     }
 }
